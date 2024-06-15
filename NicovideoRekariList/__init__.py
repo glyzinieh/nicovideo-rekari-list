f7a30e21149aae529849eae046feb1457722bf64
glyzinieh <glyzinie.h@gmail.com>
2024-06-15 14:41:38 +0900

    feat: first commit

---
+++ NicovideoRekariList/__init__.py
@@ -0,0 +1,92 @@
+import os
+
+from flask import Flask, redirect, render_template, request, send_file
+from PIL import Image, ImageDraw, ImageFont
+
+from .gspread import get_id_list, get_rekari_list, get_video_info, record_post
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def index():
+        videos = get_rekari_list()
+        return render_template("index.html", videos=videos)
+
+    @app.route("/policy/")
+    def policy():
+        return redirect("https://www.wisteriatp.work/privacy-policy")
+
+    @app.route("/search/")
+    def search():
+        return render_template("search.html")
+
+    @app.route("/watch_tmp/<string:id>")
+    def watch(id):
+        video = get_video_info(id)
+        url = f"https://www.nicovideo.jp/watch_tmp/{id}"
+        return render_template("watch_tmp.html", video=video, url=url)
+
+    @app.route("/post/", methods=["GET", "POST"])
+    def post():
+        match request.method:
+            case "GET":
+                return render_template("post.html")
+
+            case "POST":
+                id = request.form["id"]
+                title = request.form["title"]
+
+                date_data = request.form["date"].split("-")
+                date = "".join(date_data[1:])
+
+                if not id or not title or not date:
+                    msg = "ID、タイトル、日付は必須です"
+                elif id in get_id_list():
+                    msg = "この動画はすでに登録されています"
+                else:
+                    record_post(id, title, date)
+                    msg = "登録が完了しました"
+
+                return render_template("post.html", msg=msg)
+
+    @app.route("/api/thumbnail/<string:id>")
+    def thumbnail(id):
+        video = get_video_info(id)
+
+        title = video["タイトル"]
+
+        # 画像のサイズを指定
+        width, height = 1200, 630
+        # 新しい画像を作成
+        image = Image.new("RGB", (width, height), color=(73, 109, 137))
+
+        # フォントを指定（ここではデフォルトのフォントを使用）
+        # 実際には必要なフォントファイルを指定することを推奨
+        font_path = "NOTOSANSJP-REGULAR.OTF"  # 例
+        font_size = 50
+        font = ImageFont.truetype(font_path, font_size)
+
+        # ImageDrawオブジェクトを作成
+        d = ImageDraw.Draw(image)
+        # テキストの位置を計算
+        text_bbox = d.textbbox((0, 0), title, font=font)
+        text_width = text_bbox[2] - text_bbox[0]
+        text_height = text_bbox[3] - text_bbox[1]
+        x = (width - text_width) / 2
+        y = (height - text_height) / 2
+        # テキストを描画
+        d.text((x, y), title, fill=(255, 255, 255), font=font)
+
+        # 出力ディレクトリを作成
+        output_dir = "temp"
+        os.makedirs(output_dir, exist_ok=True)
+
+        # 画像を保存
+        output_path = os.path.abspath(os.path.join("temp", f"{id}.png"))
+        image.save(output_path)
+
+        return send_file(output_path, mimetype="image/png")
+
+    return app
---
+++ NicovideoRekariList/gspread.py
@@ -0,0 +1,58 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+# シートからデータを取得する際に、辞書型で取得できるようにする
+def get_all_values_to_dicts(self) -> list[dict]:
+    dicts_data = list()
+    lists_data = self.get_all_values()
+    header = lists_data[0]
+    body = lists_data[1:]
+    for i in body:
+        dicts_data.append(dict(zip(header, i)))
+    return dicts_data
+
+
+setattr(gspread.Worksheet, "get_all_values_to_dicts", get_all_values_to_dicts)
+
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
+
+credentials = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+
+gc = gspread.authorize(credentials)
+spreadsheet = gc.open_by_url(
+    "https://docs.google.com/spreadsheets/d/1IIXr6q8NEUQJfAR7uvQj9YADcIfSIJoGsjdbNK0gvCM/"
+)
+
+setting_worksheet = spreadsheet.worksheet("設定")
+list_worksheet = spreadsheet.worksheet("動画一覧")
+
+start_date = int(setting_worksheet.acell("B2").value)
+end_date = int(setting_worksheet.acell("B3").value)
+
+
+def get_rekari_list():
+    videos = [
+        i
+        for i in list_worksheet.get_all_values_to_dicts()
+        if start_date <= int(i["日付"]) and int(i["日付"]) <= end_date
+    ]
+    return videos
+
+
+def get_id_list():
+    ids = [i["ID"] for i in list_worksheet.get_all_values_to_dicts()]
+    return ids
+
+
+def get_video_info(id):
+    video = [i for i in list_worksheet.get_all_values_to_dicts() if i["ID"] == id]
+    return video[0] if video else None
+
+
+def record_post(id, title, date):
+    list_worksheet.append_row([id, title, date])
+    return True
